92e0da0e6957e1b7c02a624b5e3e111501421f76
fsdevforworld <gldreamfulwork0110@gmail.com>
2022-11-30 00:37:57 -0500

    spring boot bestpractise init

---
+++ src/main/java/demo/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package demo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		System.out.println("--------my first spring framework--------");
+		return String.format("Hello %s!", name);
+	}
+
+}
